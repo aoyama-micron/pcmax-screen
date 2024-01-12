85c1167a1454071486363d44f81e569828d63991
aoyama-micron <j.aoyama@micron-inc.co.jp>
2024-01-12 16:20:29 +0900

    ⚡️修正

--- src/app/pcmax/home/header.tsx
+++ src/app/pcmax/home/header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
 	return (
 		<header className="header fixed left-0 right-0 top-0 z-50 border-b border-gray-300">
 			{/* <Image className="relative block w-full object-contain" src={`/images/_header.png`} alt="ヘッダー" fill /> */}
-			<img src="/images/_header.png" alt="ヘッダー" className="block w-full" />
+			{/* <img src="/images/_header.png" alt="ヘッダー" className="block w-full" /> */}
 		</header>
 	);
 };
