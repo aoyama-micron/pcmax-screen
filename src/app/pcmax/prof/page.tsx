be12620e1b4da0a4599bdefa69e452b699ccc907
aoyama-micron <j.aoyama@micron-inc.co.jp>
2024-01-18 13:28:29 +0900

    ✨1/18 13:30更新

--- src/app/pcmax/prof/page.tsx
+++ src/app/pcmax/prof/page.tsx
@@ -49,7 +49,7 @@ const Prof: FC<ProfProps> = (props) => {
 								<div className="relative h-100vw w-full">
 									<Image
 										className="block"
-										src={`/images/prof/prof_${item.id}.jpg`}
+										src={`/images/prof/prof_${item.id}.jpg?a`}
 										alt="Next.js"
 										priority
 										fill
